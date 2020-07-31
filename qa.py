0fcc2c3a1170c2eafafad0afc64d1b0eafb67ab6
AmanGupta03 <theamangupta27@gmail.com>
2020-07-31 22:11:11 +0530

    Initial Commit

--- qa.py
+++ qa.py
@@ -1,2 +1,6 @@
 """
 """
+
+from setup import enable_elastic_search
+
+enable_elastic_search()
---
+++ setup.py
@@ -0,0 +1,25 @@
+"""
+"""
+
+import os
+from subprocess import Popen, PIPE, STDOUT
+
+def enable_elastic_search(forced: bool = False):
+
+  print('Enable Elastic search...')
+
+  if forced: 
+    os.system('rm -r elasticsearch-7.6.2')
+    os.system('rm -r elasticsearch-7.6.2-linux-x86_64.tar.gz')
+  
+  if 'elasticsearch-7.6.2-linux-x86_64.tar.gz' not in os.listdir() or forced:
+    os.system('wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.6.2-linux-x86_64.tar.gz -q')
+    os.system('tar -xzf elasticsearch-7.6.2-linux-x86_64.tar.gz')
+    os.system('chown -R daemon:daemon elasticsearch-7.6.2')
+
+  es_server = Popen(['elasticsearch-7.6.2/bin/elasticsearch'],
+                   stdout=PIPE, stderr=STDOUT,
+                    preexec_fn=lambda: os.setuid(1)  # as daemon
+                  )
+  # wait until ES has started
+  os.system('sleep 30')
